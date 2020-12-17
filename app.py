b01c4f77d9f8e61e4f4645873cf7513edd79a9f7
dimitar-4 <dcholakov4@gmail.com>
2020-12-17 23:24:05 +0000

    Set debug to False in app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def get_genre_movies(genre_name):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
